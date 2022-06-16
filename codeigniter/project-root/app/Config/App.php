10792d53c7d4b58b1d299294c3a79a4fdb28d284
Carlos Alberto Silva Júnior <56141054+casjunior93@users.noreply.github.com>
2022-06-16 16:44:38 -0300

    Change: base_url

--- codeigniter/project-root/app/Config/App.php
+++ codeigniter/project-root/app/Config/App.php
@@ -24,8 +24,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
-    #public $baseURL = 'https://ocondensador.com/';
+    #public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'https://ocondensador.com/';
 
     /**
      * --------------------------------------------------------------------------
