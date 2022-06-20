53bb1aeeb360b2fe89a771ce9a0d7589bcfc5542
lucianaelisa <lucianafiap@gmail.com>
2022-06-20 00:24:54 -0300

    testando jquery

--- codeigniter/project-root/app/Views/dash/dashboard.php
+++ codeigniter/project-root/app/Views/dash/dashboard.php
@@ -19,6 +19,8 @@
     <link href="<?= base_url('/'); ?>/bootstrap/bootstrap.css" rel="stylesheet" />
     <link href="<?= base_url('/'); ?>/css/override.css" rel="stylesheet" />
 
+   <!-- Chamando jquery para teste de calendario-->
+
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
 <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
 <script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
