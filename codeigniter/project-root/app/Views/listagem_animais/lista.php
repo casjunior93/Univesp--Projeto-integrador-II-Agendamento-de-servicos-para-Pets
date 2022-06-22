2dd42459677633a6f012bf4466bd163f0a780dc8
Carlos Alberto Silva Júnior <56141054+casjunior93@users.noreply.github.com>
2022-06-22 10:28:10 -0300

    Change: mensagem no modal de adoção

--- codeigniter/project-root/app/Views/listagem_animais/lista.php
+++ codeigniter/project-root/app/Views/listagem_animais/lista.php
@@ -102,12 +102,12 @@
       <div class="modal-dialog">
         <div class="modal-content">
           <form>
-            <div class="modal-header d-grid">
+            <div class="modal-header">
               <h5 class="modal-title" id="animalLabel-<?= $animal['id']; ?>">Adotar Pingo</h5>
-              <p>Entre em contato com <strong><?= $animal['nome_usuario']; ?></strong></p>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
+              <p>Entre em contato com <strong><?= $animal['nome_usuario']; ?></strong></p>
               <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="email">
