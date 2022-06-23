655abc9217bfbc4eadb678184dc400fc4f7d07d9
Carlos Alberto Silva Júnior <56141054+casjunior93@users.noreply.github.com>
2022-06-23 10:19:54 -0300

    Arquivo de teste de banco de dados

---
+++ codeigniter/project-root/tests/database/DatabaseTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use CodeIgniter\Test\CIUnitTestCase;
+use CodeIgniter\Test\DatabaseTestTrait;
+use Tests\Support\Database\Seeds\ExampleSeeder;
+use Tests\Support\Models\ExampleModel;
+use App\Models\MensagemModel;
+use App\Models\UserModel;
+use App\Models\AnimalModel;
+
+/**
+ * @internal
+ */
+final class DatabaseTest extends CIUnitTestCase
+{
+    use DatabaseTestTrait;
+
+    protected $seed = ExampleSeeder::class;
+
+    public function getAllMensagens()
+    {
+        $model = new MensagemModel();
+
+        // Get every row created by ExampleSeeder
+        $objects = $model->getMensagens();
+
+        // Make sure the count is as expected
+        $this->assertCount(3, $objects);
+    }
+
+    public function getAllUsuarios()
+    {
+        $model = new UserModel();
+
+        // Get every row created by ExampleSeeder
+        $objects = $model->getUsuarios();
+
+        // Make sure the count is as expected
+        $this->assertCount(3, $objects);
+    }
+
+    public function getAllAnimais()
+    {
+        $model = new AnimalModel();
+
+        // Get every row created by ExampleSeeder
+        $objects = $model->getAnimais();
+
+        // Make sure the count is as expected
+        $this->assertCount(3, $objects);
+    }
+}
