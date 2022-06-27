82e86cc956193c03e6c494f52ee0450e42378814
lucianaelisa <lucianafiap@gmail.com>
2022-06-27 15:33:24 -0300

    footers

--- codeigniter/project-root/app/Views/listagem_clinicas/lista.php
+++ codeigniter/project-root/app/Views/listagem_clinicas/lista.php
@@ -216,23 +216,24 @@
   } ?>
   <!-- Aqui será renderizado os modais -->
 
-  <!-- Footer-->
+  
+  <!--Footer-->
   <footer class="footer py-4">
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-lg-4 text-lg-start">Copyright &copy; Your Website 2022</div>
-        <div class="col-lg-4 my-3 my-lg-0">
-          <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
-          <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
-          <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-4 text-lg-start">Copyright &copy; Mascote Web 2022</div>
+                <div class="col-lg-4 my-3 my-lg-0">
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <div class="col-lg-4 text-lg-end">
+                    <a class="link-dark text-decoration-none me-3" href="https://www.univesp.br">UNIVESP - Universidade Virtual do Estado de SP </a><br></p> Projeto Integrador</p>
+                    <a class="link-dark text-decoration-none" href="#!"> Termos de uso</a>
+                </div>
+            </div>
         </div>
-
-        <div class="col-lg-4 text-lg-end">
-          <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
-          <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
-        </div>
-      </div>
-    </div>
+    </footer>
   </footer>
 
   <!-- Scripts da pasta public -->
