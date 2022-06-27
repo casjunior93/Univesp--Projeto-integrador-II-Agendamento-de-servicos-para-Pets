fc3420fe0699dac9bc037b9327b6b0b7d91e2eb8
Marcelo Henrique Goncalves <billmask@gmail.com>
2022-06-27 00:06:42 +0000

    adicionado API para pegar cidade estado a partir do cep

--- codeigniter/project-root/app/Views/auth/cadastre-se.php
+++ codeigniter/project-root/app/Views/auth/cadastre-se.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="utf-8" />
@@ -12,74 +12,6 @@
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 
-        <script>
-
-        $(document).ready(function() {
-
-            function limpa_formulário_cep() {
-                // Limpa valores do formulário de cep.
-                $("#rua").val("");
-                $("#bairro").val("");
-                $("#cidade").val("");
-                $("#uf").val("");
-                $("#ibge").val("");
-            }
-            
-            //Quando o campo cep perde o foco.
-            $("#cep").blur(function() {
-
-                //Nova variável "cep" somente com dígitos.
-                var cep = $(this).val().replace(/\D/g, '');
-
-                //Verifica se campo cep possui valor informado.
-                if (cep != "") {
-
-                    //Expressão regular para validar o CEP.
-                    var validacep = /^[0-9]{8}$/;
-
-                    //Valida o formato do CEP.
-                    if(validacep.test(cep)) {
-
-                        //Preenche os campos com "..." enquanto consulta webservice.
-                        $("#rua").val("...");
-                        $("#bairro").val("...");
-                        $("#cidade").val("...");
-                        $("#uf").val("...");
-                        $("#ibge").val("...");
-
-                        //Consulta o webservice viacep.com.br/
-                        $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
-
-                            if (!("erro" in dados)) {
-                                //Atualiza os campos com os valores da consulta.
-                                $("#rua").val(dados.logradouro);
-                                $("#bairro").val(dados.bairro);
-                                $("#cidade").val(dados.localidade);
-                                $("#uf").val(dados.uf);
-                                $("#ibge").val(dados.ibge);
-                            } //end if.
-                            else {
-                                //CEP pesquisado não foi encontrado.
-                                limpa_formulário_cep();
-                                alert("CEP não encontrado.");
-                            }
-                        });
-                    } //end if.
-                    else {
-                        //cep é inválido.
-                        limpa_formulário_cep();
-                        alert("Formato de CEP inválido.");
-                    }
-                } //end if.
-                else {
-                    //cep sem valor, limpa formulário.
-                    limpa_formulário_cep();
-                }
-            });
-        });
-    </script>
-
-
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
     <link href="<?= base_url('/'); ?>/css/modal.css" />
@@ -88,7 +20,7 @@
 
     <script src="<?= base_url('/'); ?>/js/jquery-3.6.0.min.js"></script>
     <script src="<?= base_url('/'); ?>/js/scripts.js"></script>
-
+    <script src="<?= base_url('/'); ?>/js/fill_form.js"></script>
 
 </head>
 
@@ -149,7 +81,7 @@
 
                         <div class="form-group">
                             <label for="inputAddress">Cep<span class="text-danger">*</span></label>
-                            <input type="text" class="form-control" id="inputAddressCep" name="cep" id="cep" value="<?= set_value('cep'); ?>" required>
+                            <input type="text" class="form-control" id="inputAddressCep" name="cep" value="<?= set_value('cep'); ?>" required>
                             <?php
                             if (isset($validation) && mostra_erro($validation, 'cep') != '') {
                                 echo '<div class="alert alert-danger">';
@@ -160,7 +92,7 @@
                         </div>
                         <div class="form-group">
                             <label for="inputAddress">Rua/ Avenida<span class="text-danger">*</span> </label>
-                            <input type="text" class="form-control" id="inputAddressRua" name="rua" id="rua" value="<?= set_value('rua'); ?>" required>
+                            <input type="text" class="form-control" id="inputAddressRua" name="rua" value="<?= set_value('rua'); ?>" required>
                             <?php
                             if (isset($validation) && mostra_erro($validation, 'rua') != '') {
                                 echo '<div class="alert alert-danger">';
@@ -171,7 +103,7 @@
                             <div class="form-row">
                                 <div class="form-group col-md-6">
                                     <label for="inputCity">Cidade<span class="text-danger">*</span></label>
-                                    <input type="text" class="form-control" id="inputCity" name="cidade" id="cidade" value="<?= set_value('cidade'); ?>" required>
+                                    <input type="text" class="form-control" id="inputCity" name="cidade" value="<?= set_value('cidade'); ?>" required>
                                     <?php
                                     if (isset($validation) && mostra_erro($validation, 'cidade') != '') {
                                         echo '<div class="alert alert-danger">';
@@ -183,7 +115,7 @@
 
                                 <div class="form-group col-md-4">
                                     <label for="inputState">Estado<span class="text-danger">*</span></label>
-                                    <select id="estado" name="estado" class="form-control" required>
+                                    <select id="inputState" name="estado" class="form-control" required>
                                         <option selected hidden>Escolha...</option>
                                         <option value="AC">Acre</option>
                                         <option value="AL">Alagoas</option>
