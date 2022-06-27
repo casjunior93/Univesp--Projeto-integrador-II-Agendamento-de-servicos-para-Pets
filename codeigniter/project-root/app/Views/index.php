3413ed4a03ae8248748459450b2d8a934fe02429
lucianaelisa <lucianafiap@gmail.com>
2022-06-27 13:56:15 -0300

    index head

--- codeigniter/project-root/app/Views/index.php
+++ codeigniter/project-root/app/Views/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta name="description" content="Gerenciador de agenda e adoção de animais para clínicas" />
+    <meta name="author" content="Equipe projeto integrador Univesp" />
     <title>Home | Mascote web</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
